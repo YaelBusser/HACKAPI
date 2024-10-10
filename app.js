a99c6015884aa3d4d618625c2a2f851a66a547c6
Yaël Busser <mrmaclevine@gmail.com>
2024-10-10 12:06:27 +0200

    ajout de la configuration requise pour la connexion à la base de données

--- app.js
+++ app.js
@@ -12,9 +12,16 @@ configDotenv();
 
 // Configuration initiale
 const app = express();
-const port = config.portApp;
+const port = config.PORT_APP;
 const server = http.createServer(app);
-const sequelize = new Sequelize(config.production);
+const sequelize = new Sequelize({
+    host: config.development.DB_HOST,
+    dialect: config.development.DB_DIALECT,
+    port: config.development.DB_PORT,
+    database: config.development.DB_NAME,
+    username: config.development.DB_USER,
+    password: config.development.DB_PASSWORD
+});
 
 
 // CORS
