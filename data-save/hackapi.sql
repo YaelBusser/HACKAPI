b2941efcfa4b742b7ee4e3c29f77372672267b6c
Yaël Busser <mrmaclevine@gmail.com>
2024-10-31 16:46:46 +0100

    updated postmana file

--- data-save/hackapi.sql
+++ data-save/hackapi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 28 oct. 2024 à 16:24
+-- Généré le : jeu. 31 oct. 2024 à 16:42
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -29,24 +29,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `features` (
   `id` int(11) NOT NULL,
-  `label` varchar(255) NOT NULL
+  `label` varchar(255) NOT NULL,
+  `tag_route` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `features`
 --
 
-INSERT INTO `features` (`id`, `label`) VALUES
-(1, 'Email address verification tool'),
-(2, 'Spam mail (content + number of sends)'),
-(3, 'Phising service (creation of a customized phishing web page - backed by AI!)'),
-(4, 'Is my password protected ?'),
-(5, 'Retrieve all domains & sub-domains associated with a Domain Name'),
-(6, 'DDoS'),
-(7, 'Random image change'),
-(8, 'Fictitious identity generation'),
-(9, 'Person information crawler (based on first / last name)'),
-(10, 'Secured password generator');
+INSERT INTO `features` (`id`, `label`, `tag_route`) VALUES
+(1, 'Email address verification tool', 'email-existence'),
+(2, 'Spam mail (content + number of sends)', 'email-spammer'),
+(3, 'Phising service (creation of a customized phishing web page - backed by AI!)', 'phishing-service'),
+(4, 'Is my password protected ?', 'password-check'),
+(5, 'Retrieve all domains & sub-domains associated with a Domain Name', 'retrieve-all-domains'),
+(6, 'DDoS', 'ddos'),
+(7, 'Random image change', 'random-image-change'),
+(8, 'Fictitious identity generation', 'generate-fake-identity'),
+(9, 'Person information crawler (based on first / last name)', 'crawler'),
+(10, 'Secured password generator', 'secure-password-generator');
 
 -- --------------------------------------------------------
 
@@ -59,22 +60,9 @@ CREATE TABLE `logs` (
   `id_user` int(11) DEFAULT NULL,
   `description` varchar(255) NOT NULL,
   `date_log` datetime NOT NULL,
-  `id_feature` int(11) DEFAULT NULL
+  `id_feature` int(11) DEFAULT NULL,
+  `status_code` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `logs`
---
-
-INSERT INTO `logs` (`id`, `id_user`, `description`, `date_log`, `id_feature`) VALUES
-(2, 2, 'POST /user', '2024-10-11 14:25:19', NULL),
-(3, 2, 'POST /user', '2024-10-11 14:42:53', NULL),
-(4, 2, 'DELETE /user', '2024-10-11 14:50:03', NULL),
-(5, 7, 'DELETE /user', '2024-10-11 14:51:33', NULL),
-(6, 7, 'DELETE /user', '2024-10-11 14:51:45', NULL),
-(7, 2, 'DELETE /user', '2024-10-11 14:51:53', NULL),
-(8, 2, 'DELETE /user', '2024-10-11 14:51:55', NULL),
-(9, 2, 'DELETE /user', '2024-10-11 14:52:01', NULL);
 
 -- --------------------------------------------------------
 
@@ -114,8 +102,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `token`, `id_role`) VALUES
-(2, 'admin', '$2a$12$zdWn01hSGFpEkLoKrzMcuuJMAvMZaVmZKADhgrJWsikx3KUpAfpUe', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJhZG1pbiIsImlhdCI6MTcyODY0NzgxOSwiZXhwIjoxNzYwMTgzODE5fQ.uHqmTBT3SsgOVpMxOYoFj-nw6jWpsiY2yaNEhUGbXzw', 1),
-(7, 'Yaël', '$2b$10$Po1jOQnHK2fYTbwWWFu21uSmCT9TQHaGjIPbVIeup6tWvMQ1eLkCi', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywidXNlcm5hbWUiOiJZYcOrbCIsImlhdCI6MTcyODY1MjE1MiwiZXhwIjoxNzYwMTg4MTUyfQ.Np9mSB2TUTm2L9PxSWTS1WRq3oBxtOoBoxiErSZLbDI', 2);
+(2, 'admin', '$2a$12$0XZloN9fX7N9YKI.jufNkehHRaiT7whYkZFaTArSUBGLn2CGlYVqe', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJhZG1pbiIsImlhdCI6MTczMDM4OTE3MSwiZXhwIjoxNzYxOTI1MTcxfQ.Il1sXznyLobu51xiLpJUelms9ywAqKfq5J4pBHAfsF0', 1),
+(7, 'Yaël', '$2a$12$0XZloN9fX7N9YKI.jufNkehHRaiT7whYkZFaTArSUBGLn2CGlYVqe', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywidXNlcm5hbWUiOiJZYcOrbCIsImlhdCI6MTczMDI5NjcyMSwiZXhwIjoxNzYxODMyNzIxfQ.O0EtKx36bz-zV3_conMjKlwK-Zrvz2_Z0xOK3h80RKE', 2);
 
 -- --------------------------------------------------------
 
@@ -143,7 +131,8 @@ ALTER TABLE `features`
 --
 ALTER TABLE `logs`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `who` (`id_user`);
+  ADD KEY `who` (`id_user`),
+  ADD KEY `id_feature` (`id_feature`);
 
 --
 -- Index pour la table `roles`
@@ -179,7 +168,7 @@ ALTER TABLE `features`
 -- AUTO_INCREMENT pour la table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=442;
 
 --
 -- AUTO_INCREMENT pour la table `roles`
@@ -191,7 +180,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Contraintes pour les tables déchargées
@@ -201,7 +190,8 @@ ALTER TABLE `users`
 -- Contraintes pour la table `logs`
 --
 ALTER TABLE `logs`
-  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `logs_ibfk_2` FOREIGN KEY (`id_feature`) REFERENCES `features` (`id`);
 
 --
 -- Contraintes pour la table `users`
